6a88d387d2c55d6ccd069bef478b94aeddd1ecac
Marcos <m.viniteixeira@gmail.com>
2012-04-18 18:47:55 -0300

    Exercicio 2

---
+++ ex2.rb
@@ -0,0 +1,5 @@
+#A comment this is so you can read your program later
+#Anything after the # is ignored by Ruby.
+puts "I could have code like this." #and the comment after is ignored
+#comentario
+puts "This will run."
