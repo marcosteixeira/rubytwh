48cd77ad69631a28bf27c90b23eeee0e312abe01
Marcos Teixeira <vini3477@hotmail.com>
2012-04-25 21:16:18 -0300

    Exercicios 7 , 8  , 9 e 10

---
+++ ex9.rb
@@ -0,0 +1,14 @@
+# Here's some new strange stuff, remember type it exactly
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+puts "Here are the days: ", days
+puts "Here are the months: ", months
+
+puts <<PARAGRAPH
+There's something going on here.
+We'll the PARAGRAPH thing
+We'll be able to type as much as we like
+Even4 line if we want, or 5, or 6.
+PARAGRAPH
